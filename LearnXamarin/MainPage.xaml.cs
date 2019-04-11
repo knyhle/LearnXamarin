97dee5d773d56eaa1220ca64b9ce949db5c41ba3
Kenny Le <kennyle1412@gmail.com>
2019-04-11 01:52:59 -0500

    initial commit

---
+++ LearnXamarin/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LearnXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Content = new Label
+            {
+                Text = "Hello World!!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.AntiqueWhite
+            };
+            this.BackgroundColor = Color.BlueViolet;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# LearnXamarin
+Repository for a small project to Learn more C# / Xamarin
+
+# WIP
+
+# License
+MIT License
